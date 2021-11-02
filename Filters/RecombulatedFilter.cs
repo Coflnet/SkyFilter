93b00ee116fd22e792f820abd31bbb4887836f91
Matthias Luger <malua007@live.de>
2021-11-02 20:05:14 +0100

    changed type of recombobulated filter

--- Filters/RecombulatedFilter.cs
+++ Filters/RecombulatedFilter.cs
@@ -7,7 +7,7 @@ namespace Coflnet.Sky.Filter
 {
     public class RecombobulatedFilter : GeneralFilter
     {
-        public override FilterType FilterType => FilterType.Equal | FilterType.SIMPLE;
+        public override FilterType FilterType => FilterType.BOOLEAN;
 
         public override IEnumerable<object> Options => new object[] { "true", "false" };
 
