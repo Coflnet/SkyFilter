a672fcafeb1f63946058bd00460bef1b452bd867
Äkwav <xekwavx@gmail.com>
2021-11-27 15:04:46 +0100

    add any pet skin option

--- Filters/PetSkinFilter.cs
+++ Filters/PetSkinFilter.cs
@@ -9,13 +9,15 @@ namespace Coflnet.Sky.Filter
     {
         public override FilterType FilterType => FilterType.Equal;
 
-        public override IEnumerable<object> Options => ItemDetails.Instance.TagLookup.Keys.Where(k => k.StartsWith("PET_SKIN"));
+        public override IEnumerable<object> Options => ItemDetails.Instance.TagLookup.Keys.Where(k => k.StartsWith("PET_SKIN")).Prepend("any");
 
         public override IQueryable<SaveAuction> AddQuery(IQueryable<SaveAuction> query, FilterArgs args)
         {
-            var item = ItemDetails.Instance.GetItemIdForName(args.Get(this));
             var key = NBT.GetLookupKey("skin");
-            return query.Include(a => a.NBTLookup).Where(a => a.NBTLookup.Where(l => l.KeyId == key && l.Value == item).Any());
+            if (args.Get(this) == "any")
+                return query.Where(a => a.NBTLookup.Where(l => l.KeyId == key).Any());
+            var item = ItemDetails.Instance.GetItemIdForName(args.Get(this));
+            return query.Where(a => a.NBTLookup.Where(l => l.KeyId == key && l.Value == item).Any());
         }
     }
 }
