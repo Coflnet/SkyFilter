fe0ba97d49d7360f40595531b4080a7e2b673ff4
Äkwav <xekwavx@gmail.com>
2021-12-27 19:30:04 +0100

    restrict rarity to pets, weapons and armor

--- Filters/RarityFilter.cs
+++ Filters/RarityFilter.cs
@@ -10,7 +10,10 @@ namespace Coflnet.Sky.Filter
         public override FilterType FilterType => FilterType.Equal ;
         public override IEnumerable<object> Options => Enum.GetNames(typeof(Tier));
 
-        public override Func<DBItem, bool> IsApplicable => item => item.Category != Category.BLOCKS;
+        public override Func<DBItem, bool> IsApplicable => item => 
+                    (item?.Tag?.StartsWith("PET_") ?? false) 
+                    || item.Category != Category.WEAPON 
+                    || item.Category == Category.ARMOR;
 
         public override IQueryable<SaveAuction> AddQuery(IQueryable<SaveAuction> query, FilterArgs args)
         {
