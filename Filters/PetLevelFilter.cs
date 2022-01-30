08c1eceada796ff3d15c74f54360c27663423e0d
Äkwav <xekwavx@gmail.com>
2022-01-30 17:46:11 +0100

    fix filter -_-

--- Filters/PetLevelFilter.cs
+++ Filters/PetLevelFilter.cs
@@ -12,10 +12,10 @@ namespace Coflnet.Sky.Filter
     {
         public override FilterType FilterType => FilterType.Equal | FilterType.NUMERICAL | FilterType.RANGE;
         public override IEnumerable<object> Options => new object[] { 1, 200 };
-
+        private static IFilter petLvlFilter = new PetLevelFilter();
         public override Expression<Func<SaveAuction, bool>> GetExpression(FilterArgs args)
         {
-            var level = args.Get(this).Replace("X", "_").Replace("x", "_");
+            var level = args.Get(petLvlFilter).Replace("X", "_").Replace("x", "_");
             if (!new Regex(@"^(1?[\dxX_]{1,2}|200)$").IsMatch(level))
                 throw new CoflnetException("invalid_pet_level", "The pased pet level is invalid. Only numbers from 1-200");
             if (args.TargetsDB)
