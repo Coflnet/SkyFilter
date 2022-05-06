9563f9af1b449baf8c175c560847f5159740f094
ekwav <ekwav@coflnet.com>
2022-05-06 17:17:52 +0200

    don't overwrite applicable

--- Filters/EditionFilter.cs
+++ Filters/EditionFilter.cs
@@ -4,8 +4,6 @@ namespace Coflnet.Sky.Filter
 {
     public class EditionFilter : NBTNumberFilter
     {
-        public override Func<DBItem, bool> IsApplicable => a
-            => true;
         protected override string PropName => "edition";
     }
 }
