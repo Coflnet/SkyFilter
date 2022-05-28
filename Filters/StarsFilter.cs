8d75aa77f8fcaf8209532256c465388e6a0e72c0
Äkwav <ekwav@coflnet.com>
2022-05-28 13:25:21 +0200

    fix star filter

--- Filters/StarsFilter.cs
+++ Filters/StarsFilter.cs
@@ -9,7 +9,7 @@ namespace Coflnet.Sky.Filter
     public class StarsFilter : NBTNumberFilter
     {
         public override IEnumerable<object> Options => new object[] { "none", "10" };
-        
+
         protected override string PropName => "dungeon_item_level";
 
         public override Expression<Func<SaveAuction, bool>> GetExpression(FilterArgs args)
@@ -17,9 +17,17 @@ namespace Coflnet.Sky.Filter
             // backwards compatable with `none`
             var key = NBT.Instance.GetKeyId(PropName);
             var stringVal = args.Get(this);
+            var newKey = NBT.Instance.GetKeyId("upgrade_level");
             if (int.TryParse(stringVal, out int val) || ContainsRangeRequest(stringVal))
                 return base.GetExpression(args);
-            return a => !a.NBTLookup.Where(l => l.KeyId == key).Any();
+            return a => !a.NBTLookup.Where(l => l.KeyId == key || l.KeyId == newKey).Any();
+        }
+
+        public override Expression<Func<SaveAuction, long>> GetSelector(FilterArgs args)
+        {
+            var key = NBT.Instance.GetKeyId(PropName);
+            var key2 = NBT.Instance.GetKeyId("upgrade_level");
+            return a => a.NBTLookup.Where(l => l.KeyId == key || l.KeyId == key2).Select(l => l.Value).FirstOrDefault();
         }
     }
 }
