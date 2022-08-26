a5c7b42134494dae834adf6d75a58a374eb3787d
Äkwav <ekwav@coflnet.com>
2022-08-26 20:16:53 +0200

    fix spider kills filter

--- Filters/SpiderKillsFilter.cs
+++ Filters/SpiderKillsFilter.cs
@@ -5,8 +5,6 @@ namespace Coflnet.Sky.Filter
 {
     public class SpiderKillsFilter : NBTNumberFilter
     {
-        public override Func<Coflnet.Sky.Items.Client.Model.Item, bool> IsApplicable => a
-            => a.Tag.StartsWith("TARANTULA_") || a.Tag.StartsWith("RECLUSE_");
         protected override string PropName => "spider_kills";
     }
 }
