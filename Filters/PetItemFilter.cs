2c08fae312f80e7dfe2553eb11f41269693160d5
Äkwav <ekwav@coflnet.com>
2023-01-27 17:26:08 +0100

    fix #36

--- Filters/PetItemFilter.cs
+++ Filters/PetItemFilter.cs
@@ -1,23 +1,33 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Microsoft.EntityFrameworkCore;
 using Coflnet.Sky.Core;
 using System.Linq.Expressions;
 
-namespace Coflnet.Sky.Filter
+namespace Coflnet.Sky.Filter;
+public class PetItemFilter : PetFilter
 {
-    public class PetItemFilter : PetFilter
-    {
-        public override FilterType FilterType => FilterType.Equal;
-        public override IEnumerable<object> Options => ItemDetails.Instance.TagLookup.Keys.Where(k => k.StartsWith("PET_ITEM"))
-            .Append("DWARF_TURTLE_SHELMET").Append("MINOS_RELIC").Append("CROCHET_TIGER_PLUSHIE");
+    public override FilterType FilterType => FilterType.Equal;
+    public override IEnumerable<object> Options => ItemDetails.Instance.TagLookup.Keys.Where(k => k.StartsWith("PET_ITEM"))
+        .Append("DWARF_TURTLE_SHELMET")
+        .Append("MINOS_RELIC")
+        .Append("CROCHET_TIGER_PLUSHIE")
+        .Append("NOT_TIER_BOOST")
+        .Prepend("any").Append("none");
 
-        public override Expression<Func<SaveAuction, bool>> GetExpression(FilterArgs args)
+    public override Expression<Func<SaveAuction, bool>> GetExpression(FilterArgs args)
+    {
+        var item = ItemDetails.Instance.GetItemIdForTag(args.Get(this));
+        var key = NBT.Instance.GetKeyId("heldItem");
+        if (args.Get(this) == "any" || string.IsNullOrEmpty(args.Get(this)))
+            return a => a.NBTLookup.Where(l => l.KeyId == key).Any();
+        if (args.Get(this) == "none")
+            return a => !a.NBTLookup.Where(l => l.KeyId == key).Any();
+        if (args.Get(this) == "NOT_TIER_BOOST")
         {
-            var item = ItemDetails.Instance.GetItemIdForTag(args.Get(this));
-            var key = NBT.Instance.GetKeyId("heldItem");
-            return a => a.NBTLookup.Where(l => l.KeyId == key && l.Value == item).Any();
+            var tierBoostId = ItemDetails.Instance.GetItemIdForTag("PET_ITEM_TIER_BOOST");
+            return a => !a.NBTLookup.Where(l => l.KeyId == key && l.Value == tierBoostId).Any();
         }
+        return a => a.NBTLookup.Where(l => l.KeyId == key && l.Value == item).Any();
     }
 }
