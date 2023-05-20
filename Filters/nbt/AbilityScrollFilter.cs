1238b85c86c52ed94bff964b7705a0630f782dd6
Äkwav <ekwav@coflnet.com>
2023-05-20 10:17:56 +0200

    ignore old scroll format

--- Filters/nbt/AbilityScrollFilter.cs
+++ Filters/nbt/AbilityScrollFilter.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Coflnet.Sky.Filter
 {
     public class AbilityScrollFilter : NBTFilter
     {
+        public override IEnumerable<object> OptionsGet(OptionValues options)
+        {
+            return options.Options.GetValueOrDefault(PropName, new List<string>()).Where(s=>!s.Contains("  ")).Append(None).Prepend(Any);
+        }
         protected override string PropName => "ability_scroll";
     }
 }
