d78aa101545525c23fd4082d5caa07e948504772
Äkwav <ekwav@coflnet.com>
2023-09-19 21:25:29 +0200

    switch out star key order

--- Filters/StarsFilter.cs
+++ Filters/StarsFilter.cs
@@ -8,23 +8,23 @@ namespace Coflnet.Sky.Filter
 {
     public class StarsFilter : NBTNumberFilter
     {
-        protected override string PropName => "dungeon_item_level";
+        protected override string PropName => "upgrade_level";
 
         public override Expression<Func<IDbItem, bool>> GetExpression(FilterArgs args)
         {
             // backwards compatable with `none`
             var key = NBT.Instance.GetKeyId(PropName);
             var stringVal = args.Get(this);
-            var newKey = NBT.Instance.GetKeyId("upgrade_level");
+            var oldKey = NBT.Instance.GetKeyId("dungeon_item_level");
             if (int.TryParse(stringVal, out int val) || ContainsRangeRequest(stringVal))
                 return base.GetExpression(args);
-            return a => !a.NBTLookup.Where(l => l.KeyId == key || l.KeyId == newKey).Any();
+            return a => !a.NBTLookup.Where(l => l.KeyId == key || l.KeyId == oldKey).Any();
         }
 
         public override Expression<Func<IDbItem, long>> GetSelector(FilterArgs args)
         {
             var key = NBT.Instance.GetKeyId(PropName);
-            var key2 = NBT.Instance.GetKeyId("upgrade_level");
+            var key2 = NBT.Instance.GetKeyId("dungeon_item_level");
             return a => a.NBTLookup.Where(l => l.KeyId == key || l.KeyId == key2).OrderByDescending(l=>l.KeyId == key2 ? 1 : 0).Select(l => l.Value).FirstOrDefault();
         }
     }
