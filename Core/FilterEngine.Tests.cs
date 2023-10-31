3ccd48a8de0d056548543dce7967be39d57bb634
Äkwav <ekwav@coflnet.com>
2023-10-31 19:33:17 +0100

    allow test to run slower

--- Core/FilterEngine.Tests.cs
+++ Core/FilterEngine.Tests.cs
@@ -25,7 +25,7 @@ namespace Coflnet.Sky.Filter.Tests
                     successCount++;
             }
             Assert.AreEqual(3000, successCount);
-            Assert.LessOrEqual(stopWatch.ElapsedMilliseconds, 20);
+            Assert.LessOrEqual(stopWatch.ElapsedMilliseconds, 25);
         }
         [Test]
         public void Skins()
