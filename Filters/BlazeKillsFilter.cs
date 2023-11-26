6c85f3546133f7f9bacf668644c4d40ef0c2daa9
Äkwav <ekwav@coflnet.com>
2023-11-26 19:17:32 +0100

    add alias for blaze kills to description

--- Filters/BlazeKillsFilter.cs
+++ Filters/BlazeKillsFilter.cs
@@ -1,4 +1,5 @@
 namespace Coflnet.Sky.Filter;
+[FilterDescription("aka Bulwark on frost wisp")]
 public class BlazeKillsFilter : NBTNumberFilter
 {
     protected override string PropName => "blaze_kills";
