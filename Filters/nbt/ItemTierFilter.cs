41375d128eb9f66f2ba29407fbe0934f5ee9f685
Äkwav <ekwav@coflnet.com>
2024-01-12 21:04:51 +0100

    convert item tier to number filter

--- Filters/nbt/ItemTierFilter.cs
+++ Filters/nbt/ItemTierFilter.cs
@@ -1,7 +1,7 @@
 namespace Coflnet.Sky.Filter;
 
 [FilterDescription("Changes based on dungeon floor level obtained")]
-public class ItemTierFilter : NBTFilter
+public class ItemTierFilter : NBTNumberFilter
 {
     protected override string PropName => "item_tier";
 }
