5a066651ded7e5dfe9b79168b6f7e771b1dc67bd
Äkwav <ekwav@coflnet.com>
2024-01-12 21:04:14 +0100

    correct item tier description

--- Filters/nbt/ItemTierFilter.cs
+++ Filters/nbt/ItemTierFilter.cs
@@ -1,6 +1,6 @@
 namespace Coflnet.Sky.Filter;
 
-[FilterDescription("Engine of the drill")]
+[FilterDescription("Changes based on dungeon floor level obtained")]
 public class ItemTierFilter : NBTFilter
 {
     protected override string PropName => "item_tier";
