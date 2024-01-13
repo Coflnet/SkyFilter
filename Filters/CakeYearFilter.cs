d27c575be3240ea7391dca9db15f1892c2c13c6d
Äkwav <ekwav@coflnet.com>
2024-01-13 18:11:20 +0100

    don't depend on db for cakeyear filter

--- Filters/CakeYearFilter.cs
+++ Filters/CakeYearFilter.cs
@@ -6,21 +6,15 @@ using Coflnet.Sky.Core;
 
 namespace Coflnet.Sky.Filter
 {
-    public class CakeYearFilter : NumberFilter
+    public class CakeYearFilter : NBTNumberFilter
     {
         public override IEnumerable<object> Options => new object[] { 1, CurrentMinecraftYear() +1 };
 
-        public override Func<Coflnet.Sky.Items.Client.Model.Item, bool> IsApplicable => item => item.Tag == "NEW_YEAR_CAKE";
+        protected override string PropName => "new_years_cake";
 
         private static int CurrentMinecraftYear()
         {
             return (int)((DateTime.Now - new DateTime(2019, 6, 13)).TotalDays / (TimeSpan.FromDays(5) + TimeSpan.FromHours(4)).TotalDays + 1);
-        }
-
-        public override Expression<Func<IDbItem, long>> GetSelector(FilterArgs args)
-        {
-            var key = NBT.Instance.GetKeyId("new_years_cake");
-            return a => a.NBTLookup.Where(l => l.KeyId == key).Select(l => l.Value).FirstOrDefault();
         }
     }
 }
