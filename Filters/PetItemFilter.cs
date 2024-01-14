864c6b15334ad2a1022809dd93c62e97becf1275
Äkwav <ekwav@coflnet.com>
2024-01-14 10:22:32 +0100

    fix pet item filter

--- Filters/PetItemFilter.cs
+++ Filters/PetItemFilter.cs
@@ -6,34 +6,27 @@ using System.Linq.Expressions;
 
 namespace Coflnet.Sky.Filter;
 [FilterDescription("Item held by the pet")]
-public class PetItemFilter : PetFilter
+public class PetItemFilter : NBTFilter
 {
-    public override FilterType FilterType => FilterType.Equal;
-    public override IEnumerable<object> Options => ItemDetails.Instance.TagLookup.Keys.Where(k => k.StartsWith("PET_ITEM"))
-        .Append("DWARF_TURTLE_SHELMET")
-        .Append("MINOS_RELIC")
-        .Append("CROCHET_TIGER_PLUSHIE")
-        .Prepend(NBTFilter.Any).Append(NBTFilter.None);
+    protected override string PropName => "heldItem";
+
     public override IEnumerable<object> OptionsGet(OptionValues options)
     {
-        return  options.Options.GetValueOrDefault("heldItem", new List<string>())
-            .Append(NBTFilter.None).Prepend(NBTFilter.Any)
+        return options.Options.GetValueOrDefault("heldItem", new List<string>())
+            .Append(None).Prepend(Any)
             .Append("NOT_TIER_BOOST");
     }
 
     public override Expression<Func<IDbItem, bool>> GetExpression(FilterArgs args)
     {
-        var key = NBT.Instance.GetKeyId("heldItem");
         if (args.Get(this) == "NOT_TIER_BOOST")
         {
+            if (!args.TargetsDB)
+                return a => !(a as SaveAuction).FlatenedNBT.Where(v => v.Key == PropName && v.Value == "PET_ITEM_TIER_BOOST").Any() && ItemCheck()(a as SaveAuction);
             var tierBoostId = ItemDetails.Instance.GetItemIdForTag("PET_ITEM_TIER_BOOST");
+            var key = NBT.Instance.GetKeyId("heldItem");
             return a => !a.NBTLookup.Where(l => l.KeyId == key && l.Value == tierBoostId).Any();
         }
-        if (args.Get(this) == NBTFilter.Any || string.IsNullOrEmpty(args.Get(this)))
-            return a => a.NBTLookup.Where(l => l.KeyId == key).Any();
-        if (args.Get(this) == NBTFilter.None)
-            return a => !a.NBTLookup.Where(l => l.KeyId == key).Any();
-        var item = ItemDetails.Instance.GetItemIdForTag(args.Get(this));
-        return a => a.NBTLookup.Where(l => l.KeyId == key && l.Value == item).Any();
+        return base.GetExpression(args);
     }
 }
