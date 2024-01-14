84be3b615f6775c48ed3a55de620128ec6ce2d3d
Äkwav <ekwav@coflnet.com>
2024-01-14 09:57:14 +0100

    don't care about filter name casing

--- Core/FilterDictonary.cs
+++ Core/FilterDictonary.cs
@@ -5,6 +5,13 @@ namespace Coflnet.Sky.Filter
 {
     public class FilterDictonary : Dictionary<string,IFilter>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Coflnet.Sky.Filter.FilterDictonary"/> class.
+        /// Tells the base class to compare the keys case insensitive
+        /// </summary>
+        public FilterDictonary() : base(StringComparer.OrdinalIgnoreCase)
+        {
+        }
         public void Add<TFilter>()  where TFilter : IFilter
         {
             this.Add(Activator.CreateInstance<TFilter>());
