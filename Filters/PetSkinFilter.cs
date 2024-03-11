15d6fd380294e4ce5bbc682eb358c89e6a16d969
Äkwav <ekwav@coflnet.com>
2024-03-11 20:40:10 +0100

    don't display pet skin filter on items

--- Filters/PetSkinFilter.cs
+++ Filters/PetSkinFilter.cs
@@ -10,7 +10,7 @@ namespace Coflnet.Sky.Filter
     [FilterDescription("This filter restricts applied skins to just pets. For more skin name options use the skin filter.")]
     public class PetSkinFilter : SkinFilter
     {
-        public override Func<Coflnet.Sky.Items.Client.Model.Item, bool> IsApplicable => item => PetFilter.IsPet(item) && item.Modifiers.Any(m => m.Slug == PropName);
+        public override Func<Items.Client.Model.Item, bool> IsApplicable => item => false; // don't display on items anymore
 
         public override IEnumerable<object> OptionsGet(OptionValues options)
         {
