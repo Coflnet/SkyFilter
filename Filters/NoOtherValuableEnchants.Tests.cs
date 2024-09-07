dec32899af9d118c656ffb920cdec1b2abae1d09
Äkwav <ekwav@coflnet.com>
2024-09-07 13:28:11 +0200

    Fix NoOtherValuableEnchant fix #88

---
+++ Filters/NoOtherValuableEnchants.Tests.cs
@@ -0,0 +1,23 @@
+using Coflnet.Sky.Core;
+using NUnit.Framework;
+
+namespace Coflnet.Sky.Filter;
+public class NoOtherValuableEnchantsFilterTests
+{
+    [TestCase("sharpness", Enchantment.EnchantmentType.sharpness, 6, true)]
+    [TestCase("sharpness", Enchantment.EnchantmentType.pristine, 5, false)]
+    public void Matches(string filterName, Enchantment.EnchantmentType enchantmentType, byte level, bool expected)
+    {
+        var filter = new NoOtherValuableEnchantsFilter();
+        var args = new FilterArgs(new() { { filterName, level.ToString() } }, true);
+        var expr = filter.GetBool(args);
+        var saveAuction = new SaveAuction()
+        {
+            Enchantments = new(){
+                new Enchantment(enchantmentType, level)
+            }
+        };
+        Assert.That(expr.Compile()(saveAuction), Is.EqualTo(expected));
+    }
+
+}
--- Filters/NoOtherValuableEnchantsFilter.cs
+++ Filters/NoOtherValuableEnchantsFilter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using Coflnet.Sky.Core;
@@ -12,6 +13,37 @@ public class NoOtherValuableEnchantsFilter : BoolFilter
         var allEnchants = Enum.GetValues(typeof(Enchantment.EnchantmentType)).Cast<Enchantment.EnchantmentType>().Where(e => e != Enchantment.EnchantmentType.None);
         var filternames = args.Filters.Keys;
         var otherEnchants = allEnchants.Where(e => filternames.Contains(e.ToString())).ToHashSet();
-        return a => !(a as SaveAuction).Enchantments.Where(e => !otherEnchants.Contains(e.Type) && e.Level >= 6).Any();
+        if (!args.TargetsDB)
+        {
+            var levels = Constants.RelevantEnchants.Where(r => r.Level < 6 && !otherEnchants.Contains(r.Type)).ToDictionary(r => r.Type, r => r.Level);
+            return a => !(a as SaveAuction).Enchantments.Where(e => !otherEnchants.Contains(e.Type) && e.Level >= 6 || levels.Where(o => o.Key == e.Type && o.Value <= e.Level).Any()).Any();
+        }
+        var level1List = new HashSet<Enchantment.EnchantmentType>();
+        var level2List = new HashSet<Enchantment.EnchantmentType>();
+        var level3List = new HashSet<Enchantment.EnchantmentType>();
+        var level4List = new HashSet<Enchantment.EnchantmentType>();
+        var level5List = new HashSet<Enchantment.EnchantmentType>();
+        foreach(var enchant in Constants.RelevantEnchants)
+        {
+            if (otherEnchants.Contains(enchant.Type))
+                continue;
+            if (enchant.Level == 1)
+                level1List.Add(enchant.Type);
+            else if (enchant.Level == 2)
+                level2List.Add(enchant.Type);
+            else if (enchant.Level == 3)
+                level3List.Add(enchant.Type);
+            else if (enchant.Level == 4)
+                level4List.Add(enchant.Type);
+            else if (enchant.Level == 5)
+                level5List.Add(enchant.Type);
+        }
+        return a => !(a as SaveAuction).Enchantments.Where(e => !otherEnchants.Contains(e.Type) && e.Level >= 6 
+        || level1List.Contains(e.Type) && e.Level >= 1
+        || level2List.Contains(e.Type) && e.Level >= 2
+        || level3List.Contains(e.Type) && e.Level >= 3
+        || level4List.Contains(e.Type) && e.Level >= 4
+        || level5List.Contains(e.Type) && e.Level >= 5
+        ).Any();
     }
 }
