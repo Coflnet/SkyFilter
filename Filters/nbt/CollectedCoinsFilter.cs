fd3dcc267a228d462a67189883adf2515967f4bc
Äkwav <ekwav@coflnet.com>
2024-10-03 12:37:00 +0200

    Correct Coins Collected to number filter

--- Filters/nbt/CollectedCoinsFilter.cs
+++ Filters/nbt/CollectedCoinsFilter.cs
@@ -1,7 +1,7 @@
 namespace Coflnet.Sky.Filter;
 
 [FilterDescription("Coins collected on Crown of Avarice")]
-public class CollectedCoinsFilter : NBTFilter
+public class CollectedCoinsFilter : NBTNumberFilter
 {
     protected override string PropName => "collected_coins";
 }
