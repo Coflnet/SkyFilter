36b61c4147425bc98c684e11870eeda8d0475fa5
Äkwav <ekwav@coflnet.com>
2024-12-12 16:12:04 +0100

    extend filter to limit db size

--- Filters/EndAfterFilter.cs
+++ Filters/EndAfterFilter.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Linq.Expressions;
 using Coflnet.Sky.Core;
+using Moq;
 
 namespace Coflnet.Sky.Filter;
 public class EndAfterFilter : DateTimeFilter
 {
     protected override Expression<Func<IDbItem, bool>> GetComparison(DateTime timestamp)
     {
+        if (timestamp > DateTime.UtcNow - TimeSpan.FromDays(15) && timestamp < DateTime.UtcNow - TimeSpan.FromHours(1))
+        {
+            return a => (a as SaveAuction).End > timestamp && (a as SaveAuction).Id > 730_000_000;
+        }
         return a => (a as SaveAuction).End > timestamp;
     }
 }
