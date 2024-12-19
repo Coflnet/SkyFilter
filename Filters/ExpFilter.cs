b70f3bbbd63b9439a505e65b394cbf70799f326d
Äkwav <ekwav@coflnet.com>
2024-12-19 11:38:49 +0100

    don't limit max exp fix #97

--- Filters/ExpFilter.cs
+++ Filters/ExpFilter.cs
@@ -1,15 +1,9 @@
 using System;
-using System.Collections.Generic;
 
 namespace Coflnet.Sky.Filter;
 [FilterDescription("Filter by (pet) experience")]
 public class PetExpFilter : NBTNumberFilter
 {
-    public override Func<Coflnet.Sky.Items.Client.Model.Item, bool> IsApplicable => PetFilter.IsPet;
+    public override Func<Items.Client.Model.Item, bool> IsApplicable => PetFilter.IsPet;
     protected override string PropName => "exp";
-    public override IEnumerable<object> OptionsGet(OptionValues options)
-    {
-        yield return 0;
-        yield return PetLevelFilter.TotalMaxExp;
-    }
 }
