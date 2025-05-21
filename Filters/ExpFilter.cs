233b7ca2a06673206167c5cd8ede0a30141454a4
Äkwav <ekwav@coflnet.com>
2025-05-21 23:41:22 +0200

    Add static exp option since the item service does not store exp variations (they are basically always 24m) fix #107

--- Filters/ExpFilter.cs
+++ Filters/ExpFilter.cs
@@ -1,9 +1,15 @@
 using System;
+using System.Collections.Generic;
 
 namespace Coflnet.Sky.Filter;
 [FilterDescription("Filter by (pet) experience")]
 public class PetExpFilter : NBTNumberFilter
 {
+    public override IEnumerable<object> Options => [0, PetLevelFilter.TotalMaxExp * 2];
+    public override IEnumerable<object> OptionsGet(OptionValues options)
+    {
+        return Options;
+    }
     public override Func<Items.Client.Model.Item, bool> IsApplicable => PetFilter.IsPet;
     protected override string PropName => "exp";
 }
