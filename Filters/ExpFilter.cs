c6f22ea903b5f9cf5553e5b677564e389d9ee8d5
Äkwav <ekwav@coflnet.com>
2025-05-23 10:50:43 +0200

    increase upper exp limit

--- Filters/ExpFilter.cs
+++ Filters/ExpFilter.cs
@@ -5,7 +5,7 @@ namespace Coflnet.Sky.Filter;
 [FilterDescription("Filter by (pet) experience")]
 public class PetExpFilter : NBTNumberFilter
 {
-    public override IEnumerable<object> Options => [0, PetLevelFilter.TotalMaxExp * 2];
+    public override IEnumerable<object> Options => [0, (long)int.MaxValue * 4];
     public override IEnumerable<object> OptionsGet(OptionValues options)
     {
         return Options;
